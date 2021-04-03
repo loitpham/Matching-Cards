dee1d3d73d483c8ccaf4c695f3e1e0722cc56789
Loi Pham <loitpham@gmail.com>
2021-04-02 19:50:41 -0700

    Shuffled the cards

--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -24,7 +24,7 @@ struct MemoryGame<CardContent> {
             cards.append(Card(id: pairIndex * 2, content: content))
             cards.append(Card(id: pairIndex * 2 + 1, content: content))
         }
-//        cards.shuffle()
+        cards.shuffle()
     }
     
     func choose(card: Card) {
